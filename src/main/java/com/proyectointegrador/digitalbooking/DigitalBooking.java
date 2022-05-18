362b1acdd7a60ef52d93476ecd468e63167a9548
EnzoFerreira4152 <enzo_al_17@hotmail.com>
2022-05-18 18:23:49 -0300

    Initial structure + API creation

--- src/main/java/com/proyectointegrador/digitalbooking/DigitalBooking.java
+++ src/main/java/com/proyectointegrador/digitalbooking/DigitalBooking.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DigitalBooking {
 
 	public static void main(String[] args) {
+
+		PropertyConfigurator.configure("log4j.properties");
 		SpringApplication.run(DigitalBooking.class, args);
 	}
 
