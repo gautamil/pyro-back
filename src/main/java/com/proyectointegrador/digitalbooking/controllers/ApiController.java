c4d7595f11dfc2265b4e72b395a0cedc01a37230
Cam Gauto <63105862+gautamil@users.noreply.github.com>
2022-05-19 15:47:10 -0300

    Adding controller + minor corrections to service

--- src/main/java/com/proyectointegrador/digitalbooking/controllers/ApiController.java
+++ src/main/java/com/proyectointegrador/digitalbooking/controllers/ApiController.java
@@ -1,8 +1,48 @@
 package com.proyectointegrador.digitalbooking.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.proyectointegrador.digitalbooking.dto.CategoryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.ICategoryService;
+
+import java.util.Collection;
+
 @CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/categories")
 public class ApiController {
+
+    @Autowired
+    ICategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity createCategory(@RequestBody CategoryDTO categoryDTO){
+        categoryService.createCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Category created");
+    }
+
+    /*@GetMapping("/{id}")
+    public CategoryDTO getCategory(@PathVariable Float id){
+        return categoryService.findCategory(id);
+    }*/
+
+    @PutMapping
+    public ResponseEntity<?> editCategory(@RequestBody CategoryDTO categoryDTO){
+        categoryService.editCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Category edited");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeCategory(@PathVariable Float id){
+        categoryService.removeCategory(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Category deleted");
+    }
+
+    @GetMapping
+    public Collection<CategoryDTO> getAllCategories(){
+        return categoryService.getCategories();
+    }
+
 }
